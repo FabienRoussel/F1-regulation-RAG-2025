b2d72e1cb940430491738f8bc00e57401960d631
PideurM <mrpideurm@gmail.com>
2025-08-12 22:15:35 +0200

    FEAT: New function to split pdf by each regulation chapter

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 import pandas as pd
 
-from src.processing.pdf_processing import extract_pdf_text_to_dataframe
+from src.processing.pdf_processing import extract_pdf_text_to_dataframe, extract_sections_to_dataframe
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
@@ -10,6 +10,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 pdf_filename = 'FIA 2025 Formula 1 Sporting Regulations - Issue 5 - 2025-04-30.pdf'
 pdf_path = os.path.join(os.path.dirname(__file__), 'data', 'pdfs', pdf_filename)
+processed_data_dir = os.path.join(os.path.dirname(__file__), 'data', 'processed')
+
+# Splitting the PDF into pages and extracting text
 df = extract_pdf_text_to_dataframe(pdf_path)
-df.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'processed', 'pdf_text.csv'), index=False)
-print(df['Text'][1])
+df.to_csv(os.path.join(processed_data_dir, 'pdf_text.csv'), index=False)
+
+# Extracting each section from the text and saving to a DataFrame, except appendices
+df_sections = pd.concat([extract_sections_to_dataframe(page) for page in df['Text']], ignore_index=True)
+df_sections.to_csv(os.path.join(processed_data_dir, 'pdf_sections.csv'), index=False)
--- src/processing/pdf_processing.py
+++ src/processing/pdf_processing.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+
 from langchain_community.document_loaders import PyMuPDFLoader
 from src.utils.utils import remove_footer
 
@@ -27,3 +29,30 @@ def extract_pdf_text_to_dataframe(pdf_path: str) -> pd.DataFrame:
     except Exception as e:
         raise RuntimeError(f"Error processing PDF file: {e}")
 
+def extract_sections_to_dataframe(text: str) -> pd.DataFrame:
+    """
+    Extracts sections from the text based on patterns like 'NUMBER) A TITLE'
+    and returns a DataFrame with 'title' and 'chunk' columns.
+
+    Args:
+        text (str): The text to process.
+
+    Returns:
+        pd.DataFrame: A DataFrame with 'title' and 'chunk' columns.
+    """
+    # Updated regex to handle cases like "VIRTUAL SAFETY CAR (VSC)"
+    # and exclude titles with letters before the number
+    section_pattern = r"(?<![A-Z])(\d+\)\s+(?:[A-Z ,\n\-\(\)É]+))\n"
+    matches = re.split(section_pattern, text)
+
+    titles = []
+    chunks = []
+
+    for i in range(1, len(matches), 2):
+        titles.append(matches[i].replace('\n', ' ').strip())  # Clean multi-line titles
+        chunks.append(matches[i + 1].strip())
+
+    processed_df = pd.DataFrame({'title': titles, 'chunk': chunks})
+    processed_df[['title', 'chunk']] = processed_df[['title', 'chunk']].map(lambda x: x.replace('\n', ' ').strip())
+
+    return processed_df
